0130f950e0a084059803476f8f0190e9de9ab067
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-17 12:58:59 +0530

    [Shantanu] Added : Create contact in Address Book.

---
+++ Contacts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class Contacts
+    {
+        public string FirstName, LastName, Address, City;
+        public string State, ZipCode, PhoneNumber, EmailId;
+    }
+}
---
+++ UC_1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class UC_1
+    {
+        public static List<Contacts> AddressBook = new List<Contacts>();
+
+        public void UserInput()
+        {
+            Contacts contacts = new Contacts();
+
+            Console.Write("Enter First Name: ");
+            contacts.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            contacts.LastName = Console.ReadLine();
+
+            Console.Write("Enter Your Address: ");
+            contacts.Address = Console.ReadLine();
+
+            Console.Write("Enter Your City Name: ");
+            contacts.City = Console.ReadLine();
+
+            Console.Write("Enter State Name: ");
+            contacts.State = Console.ReadLine();
+
+            while(true)
+            {
+                Console.Write("Enter Zip Code of your area: ");
+                string code = Console.ReadLine();
+
+                if (code.Length == 6)
+                {
+                    contacts.ZipCode = code;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid 6 digit Zip Code.");
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Enter Your Phone Number: ");
+                string number = Console.ReadLine();
+
+                if (number.Length == 10)
+                {
+                    contacts.PhoneNumber = number;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid 10 digit Phone Number.");
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Enter Your Email Address: ");
+                string mail = Console.ReadLine();
+
+                if (mail.Contains("@"))
+                {
+                    contacts.EmailId = mail;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid Email Address.");
+                }
+            }
+            AddressBook.Add(contacts);
+
+            Console.WriteLine("--------> Added Successfully <--------");
+        }
+    }
+}
