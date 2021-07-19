7d9e0e13ac3374e0dc66ed4dc89290f92b7cd636
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-19 13:19:31 +0530

    [Shantanu] Added : Class to Add Multiple person contacts.

---
+++ UC_5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class UC_5
+    {
+        public void AddingMultiple()
+        {
+            Console.WriteLine("Enter how many contacts you want to add: ");
+            int numberOfContacts = int.Parse(Console.ReadLine());
+
+            int i = 1;
+
+            while (i <= numberOfContacts) 
+            {
+                UC_1 multi = new UC_1();
+                multi.UserInput();
+                i++;
+                Console.WriteLine("{0} contact is added.",i-1);
+            }
+
+        }
+    }
+}
