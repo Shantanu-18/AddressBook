9f857bbfb72f9478f4789ddfaf5ee02149b2638b
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-20 23:22:34 +0530

    [Shantanu] Modified : Contacts class

--- Contacts.cs
+++ Contacts.cs
@@ -6,7 +6,13 @@ namespace AddressBook
 {
     public class Contacts
     {
-        public string FirstName, LastName, Address, City;
-        public string State, ZipCode, PhoneNumber, EmailId;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailId { get; set; }
     }
 }
