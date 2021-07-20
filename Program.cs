48d7d5e0638c5d8c6cfb2913413edae58d2aedc6
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-20 23:25:46 +0530

    [Shantanu] Modified : refactor main program file by using dictionary

--- Program.cs
+++ Program.cs
@@ -8,46 +8,51 @@ namespace AddressBook
         {
             Console.WriteLine("Welcome to Address Book.");
 
+
+            UC_6 dict = new UC_6();
+            dict.AddressBookDict();
+
             while (true)
             {
-                Console.WriteLine("\nEnter your choice \n 1 for Adding Contacts \n 2 for viewing Address book" +
-                    " \n 3 for editing the existing contact \n 4 for deleting contact \n 5 for adding multiple contacts");
+                Console.WriteLine("\nEnter your choice \n 1 for viewing Address book \n 2 for editing the existing contact" +
+                   " \n 3 for deleting contact");
 
                 int choice = int.Parse(Console.ReadLine());
 
+
                 switch (choice)
                 {
+                    //case 1:
+                    //    UC_1 obj = new UC_1();
+                    //    obj.UserInput();
+                    //    break;
+
                     case 1:
-                        UC_1 obj = new UC_1();
-                        obj.UserInput();
+                        UC_2 obj1 = new UC_2();
+                        //obj1.viewing();
+                        obj1.dictview();
                         break;
 
                     case 2:
-                        UC_2 obj1 = new UC_2();
-                        obj1.viewing();
-                        break;
-
-                    case 3:
                         UC_3 obj2 = new UC_3();
                         obj2.Editing();
                         break;
 
-                    case 4:
+                    case 3:
                         UC_4 obj3 = new UC_4();
                         obj3.Delete();
                         break;
 
-                    case 5:
-                        UC_5 obj4 = new UC_5();
-                        obj4.AddingMultiple();
-                        break;
+                    //case 5:
+                    //    UC_5 obj4 = new UC_5();
+                    //    obj4.AddingMultiple();
+                    //    break;
 
                     default:
                         Console.WriteLine("Enter valid choice.");
                         break;
-
-
                 }
+
             }
         }
     }
