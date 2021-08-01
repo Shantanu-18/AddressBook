c9ebdb212f6ade47e25cfc6bf759d64a626d6364
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-01 18:46:52 +0530

    [Shantanu] Added : Class for adding multiple contacts in address book.

---
+++ AddMultiple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class AddMultiple
+    {
+        public void AddingMultiple()
+        {
+            Console.Write("Enter how many contacts you want to add: ");
+            int numberOfContacts = int.Parse(Console.ReadLine());
+
+            int i = 1;
+
+            while (i <= numberOfContacts) 
+            {
+                AddingContacts multi = new AddingContacts();
+                multi.UserInput();
+                i++;
+                Console.WriteLine("{0} contact is added.",i-1);
+            }
+
+        }
+    }
+}
