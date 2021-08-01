f627fee71854af1b220d65f0bd0703aa2a3a1538
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-01 18:50:11 +0530

    [Shantanu] Added : Class for adding multiple address books with specific names.

---
+++ MultipleBooks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class MultipleBooks
+    {
+        public static Dictionary<string, List<Contact>> addressDictionary = new Dictionary<string, List<Contact>>();
+
+        public void AddressBookDict()
+        {
+            
+            string nameOfDict;
+
+            Console.Write("\nEnter the number of address books you want create: ");
+            int numOfAddressBooks = int.Parse(Console.ReadLine());
+
+
+            for (int a = 1; a <= numOfAddressBooks; a++) 
+            {
+                Console.Write("Enter the name of Address Book: ");
+                nameOfDict = Console.ReadLine();
+
+                if (addressDictionary.Count > 0)
+                {
+                    if (addressDictionary.ContainsKey(nameOfDict))
+                    {
+                        Console.WriteLine("Name already exists.");
+                    }
+                }
+
+                AddMultiple objec = new AddMultiple();
+                objec.AddingMultiple();
+
+                addressDictionary.Add(nameOfDict, AddingContacts.AddressBook);
+            }
+        }
+    }
+}
